296e9415dcc9e5c7f3f9653ca13438797acfb4ad
Vadim Kiselev <96394339+vadim-kiselev@users.noreply.github.com>
2023-05-02 09:24:50 +0300

    Delete Say Hi as Python.py

--- Say Hi as Python.py
+++
@@ -1,6 +0,0 @@
-#damn. I've almost totally forgot how what I've learned 2y ago about Py. Need to refresh.
-name = 'Vadim'
-place = 'Github'
-lang = 'Python'
-
-print ('Hey', name, ', say Hello to ', place, ' like ', lang) 
